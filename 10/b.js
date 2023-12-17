0b04d9cd3dffcba55261b5d4bdaaf9582d109fc6
Alvin Cousins <alvin.cousins@roh.org.uk>
2023-12-17 23:24:29 +0000

    Day 10

--- 10/b.js
+++ 10/b.js
@@ -1,22 +1,134 @@
-export const part_b = (input) => {
-  const arrOfNums = input
-    .split('\n')
-    .map((ln) => ln.split(' ').map((el) => Number(el)));
+import {
+  addressToCoords,
+  coordsToAddress,
+  combineCoords,
+  isNotOutOfBounds,
+} from '../helpers/grid';
 
-  return arrOfNums
-    .map((arr) => {
-      let currentLevel = arr;
-      let isAllZero;
-      const firstNums = [arr[0]];
-      while (!isAllZero) {
-        const arrOfDiffs = currentLevel
-          .map((num, i, arr) => num - arr[i + 1])
-          .filter((num) => !isNaN(num));
-        firstNums.push(arrOfDiffs[0]);
-        currentLevel = arrOfDiffs;
-        isAllZero = arrOfDiffs.every((diff) => diff === 0);
+const cells = {};
+const visited = {};
+const neighbours = {
+  'S': [[1, 0], [-1, 0], [0, 1], [0, -1]],
+  'F': [[1, 0], [0, 1]],
+  '-': [[0, 1], [0, -1]],
+  '7': [[1, 0], [0, -1]],
+  '|': [[1, 0], [-1, 0]],
+  'L': [[0, 1], [-1, 0]],
+  'J': [[0, -1], [-1, 0]],
+  '.': [],
+};
+const queue = [];
+
+const isInside = (address) => {
+  let [row, stepsFromLeft] = addressToCoords(address);
+  stepsFromLeft--;
+  let crossingsCount = 0;
+  let ridingStart = null; // 7 || J
+
+  while (stepsFromLeft > -1) {
+    if (!visited[coordsToAddress([row, stepsFromLeft])]) {
+      stepsFromLeft--;
+      continue;
+    }
+    const curSymbol = cells[coordsToAddress([row, stepsFromLeft])].value;
+    if (ridingStart) {
+      if (
+        (ridingStart === '7' && curSymbol === 'L') ||
+        (ridingStart === 'J' && curSymbol === 'F')
+      ) {
+        ridingStart = null;
+        crossingsCount++;
+      } else if (
+        (ridingStart === '7' && curSymbol === 'F') ||
+        (ridingStart === 'J' && curSymbol === 'L')
+      ) {
+        ridingStart = null;
       }
-      return firstNums.reduce((a, c) => a + c);
-    })
-    .reduce((a, c) => a + c);
+    } else if (['7', 'J'].includes(curSymbol)) {
+      ridingStart = curSymbol;
+    }
+
+    if (curSymbol === '|') {
+      crossingsCount++;
+    }
+    stepsFromLeft--;
+  }
+
+  return crossingsCount % 2 !== 0;
+};
+
+export const part_b = (input) => {
+  const grid = input.split('\n').map((line) => line.split(''));
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[0].length; x++) {
+      cells[coordsToAddress([y, x])] = {
+        value: grid[y][x],
+        dist: grid[y][x] === 'S' ? 0 : undefined,
+      };
+
+      if (grid[y][x] === 'S') {
+        queue.push(`y:${y}_x:${x}`);
+      }
+    }
+  }
+
+  while (queue.length) {
+    const current = queue.shift();
+    const cellValue = cells[current].value;
+    const curCoords = addressToCoords(current);
+    const toCheck = neighbours[cellValue]
+      .map((coord) => combineCoords(coord, curCoords))
+      .filter(isNotOutOfBounds);
+
+    const isValid = toCheck.some((coord) => !!visited[coordsToAddress(coord)]);
+    if (!isValid && cellValue !== 'S') continue;
+
+    toCheck.forEach((check) => {
+      const checkAddress = coordsToAddress(check);
+      if (!visited[checkAddress]) {
+        queue.push(checkAddress);
+      } else {
+        cells[current].dist = cells[checkAddress].dist + 1;
+      }
+    });
+    visited[current] = true;
+  }
+
+  const startAddress = Object
+    .entries(cells)
+    .find((entry) => entry[1].value === 'S')[0];
+
+  const interfacingCells = neighbours.S.filter((n) => {
+    const neighbourCoord = combineCoords(n, addressToCoords(startAddress));
+    const neighbourSymbol = cells[coordsToAddress(neighbourCoord)].value;
+    return (n[0] === 1 && n[1] === 0 && ['|', 'J', 'L'].includes(neighbourSymbol))
+    || (n[0] === -1 && n[1] === 0 && ['|', 'F', '7'].includes(neighbourSymbol))
+    || (n[0] === 0 && n[1] === 1 && ['-', 'J', '7'].includes(neighbourSymbol))
+    || (n[0] === 0 && n[1] === -1 && ['-', 'F', 'L'].includes(neighbourSymbol))
+
+  }).map((dir) => JSON.stringify(dir));
+  let startingSymbol;
+  if (interfacingCells.includes('[1,0]')) {
+    if (interfacingCells.includes('[0,1]')) startingSymbol = 'F';
+    if (interfacingCells.includes('[0,-1]')) startingSymbol = '7';
+    if (interfacingCells.includes('[-1,0]')) startingSymbol = '|';
+  } else if (interfacingCells.includes('[-1,0]')) {
+    if (interfacingCells.includes('[0,1]')) startingSymbol = 'L';
+    if (interfacingCells.includes('[0,-1]')) startingSymbol = 'J';
+  } else {
+    startingSymbol = '-';
+  }
+  cells[startAddress].value = startingSymbol;
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[0].length; x++) {
+      const curAddress = coordsToAddress([y, x]);
+      if (!visited[curAddress] && isInside(curAddress)) {
+        cells[curAddress].isInside = true;
+      }
+    }
+  }
+
+  return Object.values(cells).reduce((a, c) => (!!c.isInside ? 1 + a : a), 0);
 };
--- 10/day10.test.js
+++ 10/day10.test.js
@@ -2,16 +2,17 @@ import { describe, expect, it } from 'bun:test';
 import { part_a } from './a';
 import { part_b } from './b';
 import test from './test.txt';
+import test_b1 from './test_b1.txt';
 import real from './real.txt';
 
 describe('day 10', () => {
   it('part a - test', () => {
     expect(part_a(test)).toEqual(4);
   });
-  console.log('part a - real:', part_a(real));
+  // console.log('part a - real:', part_a(real));
 
-  // it('part b - test', () => {
-  //   expect(part_b(test)).toEqual(2);
-  // });
+  it('part b - test', () => {
+    expect(part_b(test_b1)).toEqual(4);
+  });
   // console.log('part b - real:', part_b(real));
 });
